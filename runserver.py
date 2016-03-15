4b620a5f60108c3890ac313e9bb5d1e2e38b4e70
zxqwerxz <zhou.jeffrey.y@gmail.com>
2016-03-15 19:35:31 -0400

    runserver.py typo

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     #app.run(debug=True)
 
     # Heroku Deployment
-    port = int(os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
     # Misc
