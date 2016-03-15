1d04086ad26663df9402f05b0fd3e15c44f05a86
zxqwerxz <zhou.jeffrey.y@gmail.com>
2016-03-15 19:29:22 -0400

    Heroku settings

--- runserver.py
+++ runserver.py
@@ -2,7 +2,14 @@ import os
 from biotin_flask import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # General Testing
+    #app.run(debug=True)
+
+    # Heroku Deployment
+    port = int(os.environ.get("PORT", 5000)
+    app.run(host='0.0.0.0', port=port)
+
+    # Misc
     #app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
     #app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=False)
     #app.run()
