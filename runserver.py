6e4244d1c858dd6004aea6dd955440fcb9292f4b
tyghjkyt <eyzhou@college.harvard.edu>
2017-07-02 17:22:27 -0400

    psq module added

--- runserver.py
+++ runserver.py
@@ -3,11 +3,11 @@ from biotin_flask import app
 
 if __name__ == '__main__':
     # General Testing
-    #app.run(debug=True)
+    app.run(debug=True)
 
     # Heroku Deployment
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
 
     # Misc
     #app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
