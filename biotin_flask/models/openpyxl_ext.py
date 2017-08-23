636ee8967dfa0add0d4a303d6bddb25ce0a7077b
tyghjkyt <eyzhou@college.harvard.edu>
2017-08-23 13:36:48 -0400

    Finalized the genotyping module

--- biotin_flask/models/openpyxl_ext.py
+++ biotin_flask/models/openpyxl_ext.py
@@ -1,4 +1,9 @@
+import csv
+import os
+from openpyxl import Workbook
+
 from openpyxl.utils import column_index_from_string
+
 
 def delete_column(ws, delete_column):
     """
@@ -6,16 +11,108 @@ def delete_column(ws, delete_column):
     it still preserves the number of columns (ie. it doesn't actually
     delete any columns, but it empties one of the columns)
 
+    This code is copy-pasted from https://stackoverflow.com/questions/22048016/delete-column-from-xls-file
+    Author: John McGehee
+
     :param ws:                  (Object) Worksheet (not Workbook)
     :param delete_column:       (String) Column Letter OR (Integer) Column Number
     :return:                    (Object) Worksheet
     """
     if isinstance(delete_column, str):
         delete_column = column_index_from_string(delete_column)
-    assert delete_column >= 1, "Column numbers must be 1 or greater"
+    assert delete_column >= 1, 'Column numbers must be 1 or greater'
 
     for column in range(delete_column, ws.max_column + 1):
         for row in range(1, ws.max_row + 1):
             ws.cell(row=row, column=column).value = ws.cell(row=row, column=column+1).value
 
     return ws
+
+def wedge_column_right(ws, right, *wedge):
+    """
+    Move columns around in your worksheet!
+    This method takes columns ("wedge") and moves them so that they
+    are adjacent or to the right of another column ("right").
+    The algorithm first shifts everything right of the column ("right") one
+    column to the right. Then it assigns the values right of the
+    column ("right") to the values in columns ("wedge"). Then it deletes
+    the original columns ("wedge") using the method delete_column.
+    Columns in wedge do not need to be adjacent to each other.
+    The order that columns are provided in the args *wedge will
+    be preserved when the columns are moved.
+
+    :param ws:                  (Object) Worksheet (not Workbook)
+    :param *wedge:              (String) Column Letter OR (Integer) Column Number
+    :param right:               (String) Column Letter OR (Integer) Column Number
+    :return:                    (Object) Worksheet
+    """
+
+    wedge_list = []
+    for index,column in enumerate(wedge):
+        if isinstance(column, str):
+            wedge_list.append(column_index_from_string(column))
+        assert wedge_list[index] >= 1, 'Column numbers must be 1 or greater'
+
+    if isinstance(right, str):
+        right = column_index_from_string(right)
+    assert right >= 1, 'Column numbers must be 1 or greater'
+
+    # If wedge is already to the right of right
+    if wedge_list[0] == right + 1:
+        return ws
+
+    # Shift all columns to the right of right
+    for column in range(ws.max_column,right,-1):
+        for row in range(1, ws.max_row + 1):
+            ws.cell(row=row, column=column+len(wedge_list)).value = ws.cell(row=row, column=column).value
+
+    # Copy wedges to be adjacent to right
+    for i, column in enumerate(wedge_list):
+        for row in range(1, ws.max_row + 1):
+            ws.cell(row=row, column=right+i+1).value = ws.cell(row=row, column=column+len(wedge_list)).value
+
+    # Reverse sort wedge so that delete_column will be more efficient
+    wedge_list = sorted(wedge_list, reverse=True)
+
+    # Delete the original wedges
+    for column in wedge_list:
+        delete_column(ws, column+len(wedge_list))
+
+    return ws
+
+def csv_to_xlsx(csv_filepath):
+    """
+    This method converts csv files to .xlsx files because openpyxl can't
+    deal with csv files.
+
+    :param xls_wb:              (Path) path to csv file
+    :return:                    None
+    """
+
+    xlsx = Workbook()
+    xlsx_ws = xlsx.active
+
+    # Copy elements in csv file to xlsx Workbook
+    with open(csv_filepath, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        for num, row in enumerate(csvreader):
+            for col, element in enumerate(row):
+                # Convert numbers into integers or floats
+                if element.isdigit():
+                    element = int(element)
+                else:
+                    try:
+                        element = float(element)
+                    except:
+                        pass
+
+                xlsx_ws.cell(row=num+1,column=col+1).value = element
+
+    # Remove the old file
+    os.remove(csv_filepath)
+
+    # Save the new xlsx Workbook to the same path
+    csv_filepath = csv_filepath + 'x'
+    xlsx.save(csv_filepath)
+
+
--- biotin_flask/views/genotyping_view.py
+++ biotin_flask/views/genotyping_view.py
@@ -1,21 +1,21 @@
-"""Inputs NGS genotyping data and outputs genotyping results
+"""Inputs NGS raw genotyping data and outputs genotyping results
 
 This module parses excel sheets uploaded by the user and reads variant call frequencies per sample.
-It then outputs the genotyping results of each sample in the form of an excel sheet
+It then outputs the genotyping results of each sample and each SNP
 
-Either a single excel file or multiple excel files can be
-provided.
+Either a single excel file or multiple excel files can be provided.
 """
 
 import io, os, csv, StringIO
 from collections import OrderedDict
 
-from flask import render_template, request, flash, send_file, make_response
+from flask import render_template, request, flash, send_file
 from werkzeug import secure_filename
 from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Alignment, Font
 
 from biotin_flask import app
-from biotin_flask.models.openpyxl_ext import delete_column
+from biotin_flask.models.openpyxl_ext import delete_column, wedge_column_right, csv_to_xlsx
 
 
 @app.route('/misc/genotyping', methods=['GET', 'POST'])
@@ -30,44 +30,76 @@ def genotyping():
             An excel file
 
         """
+    # Check if there are any SNP ID lists in tmp/ref_snp_genotyping folder
+    if len(os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], 'ref_snp_genotyping'))) < 1:
+        ref_exists = False
+    else:
+        ref_exists = True
+    filenames = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], 'ref_snp_genotyping'))
 
     # Render an empty form for GET request
     if request.method == 'GET':
-        return render_template('genotyping/form.html')
+        return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
 
     # Otherwise validate the form on a POST request and process uploaded files
 
     # Obtain files from the form
     f = request.files.getlist('input')
     ref = request.files.getlist('reference')
+    ref_sel = request.form.get('reference_sel')
 
     # Throw error if one of the required files are missing
-    if not f[0] or not ref[0]:
+    if not f[0]:
         flash('A reqired field is missing', 'error')
-        return render_template('genotyping/form.html')
+        return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
+    if not ref[0] and ref_sel == 'None':
+        flash('A reqired field is missing', 'error')
+        return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
 
-    # Throw error if any file does not have .xlsx extension
+    # Validate and save variant call files
     for file in f:
         filename = secure_filename(file.filename)
         filefront, extension = os.path.splitext(filename)
-        if not extension == '.xlsx':
-            flash('Only .xlsx files are allowed.', 'error')
-            return render_template('genotyping/form.html')
-    for file in ref:
-        filename = secure_filename(file.filename)
+        if not extension == '.xls':
+            flash('Only .xls files are allowed.', 'error')
+            return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+        file.save(path)
+
+        # Convert the files in f to xlsx so that openpyxl can work with them
+        csv_to_xlsx(path)
+        extension = '.xlsx'
+        file.filename = filefront + extension
+
+    # Throw error if more than one snp list was provided
+    if ref[0] and ref_sel != 'None':
+        flash('Only one SNP ID reference file should be selected')
+        return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
+    if len(ref) > 1:
+        flash('Only one SNP ID reference file should be selected')
+        return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
+
+    if ref_sel == 'None':
+        # Validate the file object.
+        filename = secure_filename(ref[0].filename)
         filefront, extension = os.path.splitext(filename)
         if not extension == '.xlsx':
             flash('Only .xlsx files are allowed.', 'error')
-            return render_template('genotyping/form.html')
+            return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
 
-    # Save the files to the tmp folder
-    ref[0].save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(ref[0].filename)))
-    for file in f:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Save the file
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'ref_snp_genotyping', secure_filename(ref[0].filename))
+        if secure_filename(ref[0].filename) in os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], 'ref_snp_genotyping')):
+            os.remove(file_path)
+            ref[0].save(file_path)
+            list_snp = load_workbook(file_path)
+        else:
+            ref[0].save(file_path)
+            list_snp = load_workbook(file_path)
+    else:
+        list_snp = load_workbook(os.path.join(app.config['UPLOAD_FOLDER'], 'ref_snp_genotyping', ref_sel))
 
     # Open the ref file and store SNP IDs in a list
-    list_snp = load_workbook(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(ref[0].filename)))
     snp_id = []
     for row in range(1, list_snp.active.max_row + 1):
         snp_id.append(list_snp.active['A{}'.format(row)].value)
@@ -76,28 +108,65 @@ def genotyping():
     output = Workbook()
 
     # Open the input files and begin main processing loop
-    file_num = 1
     for file in f:
         file_wb = load_workbook(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
         file_ws = file_wb.active
 
-        # Deactivate the filter
-        file_ws.auto_filter.ref = None
-
         # Validate the spreadsheet
+        fields = ('Chrom', 'Position', 'Ref', 'Variant', 'Allele Call', 'Filter', 'Frequency', 'Quality', 'Filter',
+                  'Type', 'Allele Source', 'Allele Name', 'Gene ID', 'Region Name', 'VCF Position',
+                  'VCF Ref', 'VCF Variant', 'Original Coverage', 'Coverage', 'Filter', 'Coverage+',
+                  'Filter', 'Coverage-', 'Filter', 'Allele Cov', 'Allele Cov+', 'Allele Cov-', 'Strand Bias',
+                  'Filter', 'Common Signal Shift', 'Filter', 'Reference Signal Shift', 'Filter',
+                  'Variant Signal Shift', 'Filter', 'Relative Read Quality', 'Filter', 'HP Length', 'Filter',
+                  'Context Error+', 'Filter', 'Context Error-', 'Filter', 'Context Strand Bias', 'Filter',
+                  'Sample Name', 'Barcode', 'Run Name', 'Allele', 'Location')
+        for column in range(1, 50):
+            if file_ws.cell(row=1, column=column).value != fields[column-1]:
+                delete_files(f)
+                flash('{} not formatted correctly'.format(secure_filename(file.filename)))
+                return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
+
+        # Rename some of the fields
+        file_ws['C1'].value = 'Ref Allele'
+        file_ws['D1'].value = 'Var Allele'
+        file_ws['G1'].value = 'Var Frequency'
+        file_ws['L1'].value = 'SNP ID'
+        file_ws['M1'].value = 'Gene'
+        file_ws['N1'].value = 'Assay ID'
+
+        # Remove the "comma glitch" in the SNP ID column
+        for row in range(2, file_ws.max_row + 1):
+            cell = file_ws['L{}'.format(row)].value
+            next_cell = file_ws['L{}'.format(row + 1)].value
+            if cell == '---' and next_cell[:2] == 'rs':
+                try:
+                    file_ws['L{}'.format(row + 1)].value = next_cell[:next_cell.index(',')]
+                except:
+                    pass
+
+        # Delete unnecessary columns
+        del_cols = ('AX','AW','AV','AS','AR','AQ','AP','AO','AN','AM','AL','AK','AJ','AI','AH','AG','AF','AE','AD',
+                    'AC','X','V','T','R','Q','P','O','I','F')
+        for col in del_cols:
+            file_ws = delete_column(file_ws,col)
+
+        # Reorganize columns
+        file_ws = wedge_column_right(file_ws,'B','K','L','J')
+
+        # Check if the formatting worked!
         fields = ('Chrom', 'Position', 'Gene', 'Assay ID', 'SNP ID', 'Ref Allele', 'Var Allele',
                   'Allele Call', 'Var Frequency', 'Quality', 'Type', 'Allele Source', 'Coverage',
                   'Coverage+', 'Coverage-', 'Allele Cov', 'Allele Cov+', 'Allele Cov-', 'Strand Bias',
                   'Sample Name', 'Barcode')
-        for column in range(1, 21):
-            if file_ws.cell(row=1, column=column).value != fields[column-1]:
-                delete_files(f, ref)
-                flash('File not formatted correctly')
-                return render_template('genotyping/form.html')
+        for column in range(1, 22):
+            if file_ws.cell(row=1, column=column).value != fields[column - 1]:
+                delete_files(f)
+                flash('NOOO!!! Oh well.')
+                return render_template('genotyping/form.html', ref_exists=ref_exists, filenames=filenames)
 
 
-        # Calculate the genotyping result
-        # Sort the worksheet based off SNP ID
+        # Iterate through the sheet rows
         for row in range(2, file_ws.max_row + 1):
             cell = file_ws['V{}'.format(row)] # genotyping result
 
@@ -132,9 +201,8 @@ def genotyping():
         file_processed = file_wb.create_sheet('processed')
 
         # Copy first row to the new sheet
-        for column in range(1, file_ws.max_column + 1):
+        for column in range(1, 24): # There are 22 columns, including the Genotyping column
             file_processed.cell(row=1, column=column).value = file_ws.cell(row=1, column=column).value
-        file_processed.cell(row=1, column=file_processed.max_column+1).value = 'Genotyping'
 
         # Sort rows into the new processed sheet
         sort_row = 1
@@ -143,21 +211,52 @@ def genotyping():
                 if file_ws['W{}'.format(row)].value == snp:
                     sort_row += 1
                     # Copy row into new sheet
-                    for column in range(1, file_ws.max_column + 1):
-                        file_processed.cell(row=sort_row, column=column).value = file_ws.cell(row=row, column=column).value
+                    for column in range(1, 23): # This does not include the index column, column 23
+                        file_processed.cell(row=sort_row, column=column).value = file_ws.cell(row=row,
+                                                                                              column=column).value
 
-        # Delete any unnecessary columns
-        for row in range(2, file_processed.max_row + 1):
-            file_processed = delete_column(file_processed, 'W') # This is the index column
+        # Make a thick border below the sort_row
+        sort_row += 1 # Skip a row
+        end_row = sort_row # Formatting will be added in output_sheet
+
+        # Copy the rest of the rows into the new processed sheet
+            # Rows labelled 'Hotspot'
+        for row in range(2, file_ws.max_row + 1):
+            if file_ws['W{}'.format(row)].value == 'na':
+                if file_ws['L{}'.format(row)].value == 'Hotspot':
+                    sort_row += 1
+                    # Copy row into new sheet
+                    for column in range(1, 23):
+                        file_processed.cell(row=sort_row, column=column).value = file_ws.cell(row=row,
+                                                                                              column=column).value
+
+        # Copy the rest of the rows into the new processed sheet
+            # Rows labelled 'Novel'
+        for row in range(2, file_ws.max_row + 1):
+            if file_ws['W{}'.format(row)].value == 'na':
+                if file_ws['L{}'.format(row)].value == 'Novel':
+                    sort_row += 1
+                    # Copy row into new sheet
+                    for column in range(1, 23):
+                        file_processed.cell(row=sort_row, column=column).value = file_ws.cell(row=row,
+                                                                                              column=column).value
+
 
         # Create a new worksheet in output
-        output_sheet = output.create_sheet(secure_filename(file.filename))
-        file_num += 1
+        output_sheet = output.create_sheet('Variant_Calls_' + file_processed['T2'].value)
 
         # Copy the processed worksheet into output
         for row in range(1, file_processed.max_row + 1):
             for column in range(1, file_processed.max_column): # not including the last emptied column
                 output_sheet.cell(row=row, column=column).value = file_processed.cell(row=row, column=column).value
+                output_sheet.cell(row=row, column=column).alignment = Alignment(horizontal='center')
+
+        # Add the finishing formatting touches
+        output_sheet['V1'].value = 'Genotyping'
+        output_sheet.cell(row=end_row, column=1).value = 'Extra SNPs'
+        output_sheet.cell(row=end_row, column=1).font = Font(bold=True)
+        for column in range(1, 23):  # There are 22 columns, including the Genotyping column
+            output_sheet.cell(row=1, column=column).font = Font(bold=True)
 
     # Delete the first sheet of the output file
     output.remove_sheet(output.active)
@@ -168,26 +267,20 @@ def genotyping():
     output.save(os.path.join(app.config['UPLOAD_FOLDER'], 'results_genotyping.xlsx'))
 
     # Delete files from server
-    delete_files(f, ref)
+    delete_files(f)
 
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], 'results_genotyping.xlsx'), attachment_filename='results.xlsx', as_attachment=True)
 
-
-def delete_files(f, ref):
+def delete_files(f):
     """
 
     :param f:       (File) list of files
-    :param ref:     (File) list of files
-    :return:        True if successful, False is unsuccessful
+    :return:        None
     """
 
-    try:
-        for file in f:
-            filename = secure_filename(file.filename)
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        for file in ref:
-            filename = secure_filename(file.filename)
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return True
-    except:
-        return False
+    for file in f:
+        filename = secure_filename(file.filename)
+        filefront, extension = os.path.splitext(filename)
+        extension = '.xlsx'
+        filename = filefront + extension
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
