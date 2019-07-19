25730a85b30c892c4f59892832b5cf559d68ed7e
tyghjkyt <eyzhou@college.harvard.edu>
2019-07-19 16:30:07 -0400

    Updated PSQ Assay Design

--- biotin_flask/views/psq_view.py
+++ biotin_flask/views/psq_view.py
@@ -42,7 +42,7 @@ def psq():
     f = request.files.getlist('html')
 
     if not f[0]:
-        flash('A reqired field is missing', 'alert-warning')
+        flash('A required field is missing', 'alert-warning')
         return render_template('psq/form.html')
 
     # Throw error if any file does not have .html extension
@@ -154,47 +154,100 @@ def psq():
             flash('There was an error while reading file ' + secure_filename(file.filename), 'alert-warning')
             return render_template('psq/form.html')
 
+    """
+    data[0] = Assay ID
+    data[1] = Gene
+    data[2] = Assay Design Score
+    data[3] = Primer Sequence (FP)
+    data[4] = Length (FP)
+    data[5] = Tm (FP)
+    data[6] = GC% (FP)
+    data[7] = Primer Sequence (RP)
+    data[8] = Length (RP)
+    data[9] = Tm (RP)
+    data[10] = GC% (RP)
+    data[11] = Primer Sequence (FS)
+    data[12] = Length (FS)
+    data[13] = Tm (FS)
+    data[14] = GC% (FS)
+    data[15] = Amplicon Size
+    data[16] = Amplicon GC%
+    """
+
     # Prepare csv printer
     dest = StringIO.StringIO()
     writer = csv.writer(dest)
 
-    # Print header
-    writer.writerow( ["NGS#", "Gene", "Assay ID", "Length", "Tm", "GC%", "Amplicon GC%",
-                      "Amplicon size", "Assay Design Score", "Amplicon coordinates (GRCm38/mm10)",
-                      "Strand", "# CpG", "# SNP", "Primer ID", "Primer Sequences", "Ta",
-                      "Mg2+", "Note"] )
+    if request.form.get('output-format') == 'default':
 
-    # Loop through output
-    row = [''] * 18
-    for file in output:
-        row[1] = file[1]
-        row[2] = file[0]
-        row[3] = file[4]
-        row[4] = file[5]
-        row[5] = file[6]
-        row[6] = file[16]
-        row[7] = file[15]
-        row[8] = file[2]
-        row[13] = file[0] + 'FP'
-        row[14] = file[3]
-        writer.writerow( row )
-        row = [""] * 17
-        row[3] = file[8]
-        row[4] = file[9]
-        row[5] = file[10]
-        row[13] = file[0] + 'RP'
-        row[14] = file[7]
-        writer.writerow(row)
-        row = [""] * 17
-        row[3] = file[12]
-        row[4] = file[13]
-        row[5] = file[14]
-        row[13] = file[0] + seq_primer
-        row[14] = file[11]
-        writer.writerow(row)
-        row = [""] * 18
+        # Print header
+        writer.writerow( ["NGS#", "Gene", "Assay ID", "Length", "Tm", "GC%", "Amplicon GC%",
+                          "Amplicon size", "Assay Design Score", "Amplicon coordinates (GRCm38/mm10)",
+                          "Strand", "# CpG", "# SNP", "Primer ID", "Primer Sequences", "Ta",
+                          "Mg2+", "Note"] )
 
-    # Make response
-    response = make_response(dest.getvalue())
-    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
-    return response
+        # Loop through output
+        row = [''] * 18
+        for file in output:
+            row[1] = file[1] # Gene
+            row[2] = file[0] # Assay ID
+            row[3] = file[4] # Length (FP)
+            row[4] = file[5] # Tm (FP)
+            row[5] = file[6] # GC% (FP)
+            row[6] = file[16] # Amplicon GC%
+            row[7] = file[15] # Amplicon Size
+            row[8] = file[2] # Amplicon Design Score
+            row[13] = file[0] + 'FP' # Primer ID (FP)
+            row[14] = file[3] # Primer Sequence (FP)
+            writer.writerow( row )
+            row = [""] * 17
+            row[3] = file[8] # Length (RP)
+            row[4] = file[9] # Tm (RP)
+            row[5] = file[10] # GC% (RP)
+            row[13] = file[0] + 'RP' # Primer ID (RP)
+            row[14] = file[7] # Primer Sequence (RP)
+            writer.writerow(row)
+            row = [""] * 17
+            row[3] = file[12] # Length (FS)
+            row[4] = file[13] # Tm (FS)
+            row[5] = file[14] # GC% (FS)
+            row[13] = file[0] + seq_primer # Primer ID (FS)
+            row[14] = file[11] # Primer Sequence (FS)
+            writer.writerow(row)
+            row = [""] * 18
+
+        # Make response
+        response = make_response(dest.getvalue())
+        response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+        return response
+
+    if request.form.get('output-format') == 'oligo-order':
+        well_character = 'A'
+        well_number = 1
+
+        row = [""] * 7
+        writer.writerow(row)
+        writer.writerow(row)
+        writer.writerow(["Well", "Primer ID", "Primer Sequences", "", "Well", "Primer ID", "Primer Sequences"])
+
+        for file in output:
+            row[0] = well_character + str(well_number)
+            row[1] = file[0] + 'FP'
+            row[2] = file[3]
+            row[4] = well_character + str(well_number)
+            row[5] = file[0] + 'RP'
+            row[6] = file[7]
+            writer.writerow(row)
+            row = [""] * 7
+
+            # Increment the well number, e.g. A1 to A2 or A12 to B1
+            if well_number < 13:
+                well_number += 1
+            else:
+                well_number = 1
+                well_character = chr(ord(well_character) + 1)
+
+        # Make response
+        response = make_response(dest.getvalue())
+        response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+        return response
